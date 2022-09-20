83068a7037e870a85a4a9a41b0437e47b2477b8f
Julia <yagubetslulka@yandex.ru>
2022-09-20 15:43:41 +0300

    Задача1. Натуральные числа по убыванию.

---
+++ 1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N по убыванию.
+
+// M = 1; N = 5. -> ""5,4,3,2,1""
+// M = 4; N = 8. -> ""8,7,6,5,4""
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите число M");
+int numberM = int.Parse(ReadLine()!);
+WriteLine("Введите число N");
+int numberN = int.Parse(ReadLine()!);
+WriteLine(Stringnumbers(numberM, numberN));
+
+
+
+string Stringnumbers(int num1, int num2)
+{
+    if (num1 == num2) return $"{num1}";
+    else if (num1 > num2) return $"{num1} {Stringnumbers(num2, num1 - 1)}";
+    else return $"{num2} {Stringnumbers(num1, num2 - 1)}";
+
+}
